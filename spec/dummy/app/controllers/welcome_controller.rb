f44ba3e86fa87bbad490245f9e445b913bea55ef
Brady Love <love.brady@gmail.com>
2012-10-20 21:28:53 -0700

    Added links to dummy app to toggle sample toggles

--- spec/dummy/app/controllers/welcome_controller.rb
+++ spec/dummy/app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def enable
+    Togglify.enable(params[:toggle_name])
+    redirect_to root_path
+  end
+
+  def disable
+    Togglify.disable(params[:toggle_name])
+    redirect_to root_path
+  end
 end
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,8 @@
 TogglifyTestApp::Application.routes.draw do
+  resource :welcome, controller: "welcome" do
+    post 'enable'
+    post 'disable'
+  end
+
   root to: "welcome#index"
 end
